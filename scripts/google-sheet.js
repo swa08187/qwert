2830ba727b4790233e2a83d798751b184be819c6
swa08187 <swa08187@naver.com>
2025-05-27 19:52:05 +0900

    Update google-sheet.js

--- scripts/google-sheet.js
+++ scripts/google-sheet.js
@@ -4,7 +4,7 @@ var genre = document.querySelector('.genre');
 var category = document.querySelector('.category');
 var random = document.querySelector('.random');
 
-let myKey = "1B3PYJKKNa5L7EYwv4mQgpHBgmhhjIAFx2vqiCXgbyjU"; // 스프레드시트 KEY
+let myKey = "1k1YX0o71OFHrMjTsPP5xjyLYLV0RBIG4muq0mKYgW9Q"; // 스프레드시트 KEY
 
 let noCover = `https://i.namu.wiki/i/PgMGTIiIqNjYe5R56mm0yzlejxWA3l15ZrjwTJA4T03s25vH7SuWLaEvKXhG7Q_VybX0goll8IfcTrKxja7fjg.webp`;
 
