48770d4f696d888e354410de327ed6c65f30e5fc
swa08187 <swa08187@naver.com>
2025-05-31 18:40:50 +0900

    Update gsheets-parsing.js

--- scripts/gsheets-parsing.js
+++ scripts/gsheets-parsing.js
@@ -1,7 +1,7 @@
 // JS 전체 코드 - Google Sheets API v4 버전
 
 const SHEET_ID = "1i-sXORfTZXdWSDDFHvdBRd8ZyCDS7f8065af3Ou7Btg";
-const API_KEY = "1i-sXORfTZXdWSDDFHvdBRd8ZyCDS7f8065af3Ou7Btg";  // 본인 API 키 입력
+const API_KEY = "AIzaSyA13XaHj7QD6HDYVa0zgtTBE0ewihrhf8M";  // 본인 API 키 입력
 const RANGE = "Sheet1!A2:F"; // 시트이름과 범위 조정 필요
 
 const genreContainer = document.querySelector('.genre-list');
