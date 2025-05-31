f910b4ae203b6c9998adece54bb13bf4537d8b7f
swa08187 <swa08187@naver.com>
2025-05-31 18:53:56 +0900

    Update func.js

--- scripts/func.js
+++ scripts/func.js
@@ -1,4 +1,4 @@
-const API_KEY = 'AIzaSyCdwduMQkMwOoOoTGQCoVFzImSnxFg9zOY'; // 생성한 API 키로 대체
+const API_KEY = 'AIzaSyA13XaHj7QD6HDYVa0zgtTBE0ewihrhf8M'; // 생성한 API 키로 대체
 const SPREADSHEET_ID = '1i-sXORfTZXdWSDDFHvdBRd8ZyCDS7f8065af3Ou7Btg'; // 스프레드시트 ID로 대체
 const SHEET_NAME = '시트1'; // 시트 이름으로 대체
 
